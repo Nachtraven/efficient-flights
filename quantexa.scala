83c2fe6521ff4a20d8083eab9c6cd2f8bc69fe18
Sean Nachtrab <32464088+Nachtraven@users.noreply.github.com>
2020-10-05 16:34:26 +0200

    Q2 first element to solution

--- quantexa.scala
+++ quantexa.scala
@@ -5,10 +5,10 @@ import scala.util._
 object quantexa {
     def main(args: Array[String]) = {
 
-        val fileName = "flightData.csv"
+        val fileName = "flightData copy.csv"
         val dateFormat = "yyyy-MM-dd"
-        val dtf = java.time.format.DateTimeFormatter.ofPattern(dateFormat)
-
+        //val dtf = java.time.format.DateTimeFormatter.ofPattern(dateFormat)
+        //java.time.LocalDate.parse((_.split(",")(4)), dtf).getMonthValue
 
 
         //Make a list from CSV file and drop first denomination line
@@ -21,8 +21,14 @@ object quantexa {
             get_all_flights(file).groupBy(_.split(",")(4).substring(5).dropRight(3)).mapValues(_.size)
         }
 
-        //java.time.LocalDate.parse((_.split(",")(4)), dtf).getMonthValue
-        println(total_flights_month(fileName))
+        //Q2 Find the top x frequent flyers
+        def frequent_flyers(file: String, amount: Int) : Seq[(String, Int)] = {
+            get_all_flights(file).groupBy(_.split(",")(0)).mapValues(_.size).toSeq.sortWith(_._2 > _._2).toList
+        }
+
+
+        //println(total_flights_month(fileName))
+        println(frequent_flyers(fileName, 100))
     }
 }
 
